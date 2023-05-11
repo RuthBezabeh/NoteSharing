5519a898d3ca427ec1e93c4667e5b851132a5e86
Ruth Bezabeh <ruttkas@my.yorku.ca>
2023-05-10 20:04:26 -0400

    creat init and upload method for user

---
+++ user.py
@@ -0,0 +1,13 @@
+import topic
+import note
+class User:
+    def __init__(self, name):
+        self.name = name
+        self.id = 001
+        self.collectionId = 001
+    def upload(self, topic, file):
+        with open(file, 'rb') as file:
+            binaryFile = file.read()
+        #create new note object
+        #add to users collection
+        #add to topic collection
