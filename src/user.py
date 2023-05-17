c6cfd4f942155dc5806600a559e3a93f68a0a95d
Ruth Bezabeh <ruttkas@my.yorku.ca>
2023-05-17 16:31:26 -0400

    add getter methods and gui file

--- src/user.py
+++ src/user.py
@@ -3,8 +3,18 @@ import note
 class User:
     def __init__(self, name):
         self.name = name
-        self.id = 001
-        self.collectionId = 001
+        self.id = 1
+        self.collectionId = 1
+
+    def get_name(self):
+        return self.name
+    
+    def get_id(self):
+        return self.id
+    
+    def get_collection_id(self):
+        return self.collectionId
+    
     def upload(self, topic, file):
         with open(file, 'rb') as file:
             binaryFile = file.read()
